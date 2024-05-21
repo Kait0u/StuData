ac711f1b82ab5c34c6a2221b88bb611716f90b83
Kait0u <jjaw.stuff@gmail.com>
2024-05-21 20:54:15 +0200

    Updated pom.xml

--- src/test/java/pl/wit/studata/backend/models/ClassCriterionTest.java
+++ src/test/java/pl/wit/studata/backend/models/ClassCriterionTest.java
@@ -1,14 +1,33 @@
 package pl.wit.studata.backend.models;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+
+/**
+ * Klasa testowa sprawdzają poprawność działania Klasy ClassSriterion 
+ * 
+ * @author Iryna Mishchenko
+ */
 
 public class ClassCriterionTest {
 
 	@Test
 	public void testValidateScore() {
-		fail("Not yet implemented");
+		/*
+		 * Tworzymy obiekt kryteriumTestowy 
+		 * Przypisujemy do niego wartości zgodine z konstruktorem
+		 * @param string "Praca domowa"
+		 * @param int 20 -> max liczba punktów uzyskana za pracę domową 
+		 */
+		ClassCriterion kryteriumTestowy = new ClassCriterion("Praca domowa", 20);
+        assertTrue(kryteriumTestowy.validateScore(20)); //czy liczba jest w przediale od 0 do 50, wynik oczekiwany true
+        assertTrue(kryteriumTestowy.validateScore(0)); //czy liczba równa 0, wynik oczekiwany true
+        assertTrue(kryteriumTestowy.validateScore(20));//czy liczba równa 20, wynik oczekiwany true
+        assertFalse(kryteriumTestowy.validateScore(-1)); //reakcja w wypadku ujemnych pkt, wynink oczekiwany: false 
+        assertFalse(kryteriumTestowy.validateScore(21)); //reakcja w wypadku jeżeli liczba pkt większa od maksymalnej liczby, wynink oczekiwany: false
 	}
 
 }
